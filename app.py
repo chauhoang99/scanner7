d495f5736b977ae900a10ea3cb1e2ef97fbd5098
chauhoang99 <40813780+chauhoang99@users.noreply.github.com>
2026-09-11 16:57:37 +0000

    colors

--- app.py
+++ app.py
@@ -285,9 +285,9 @@ def style_row(row):
             
             # Standard sweep colors if >= 5 pips
             if "High Sweep" in val:
-                styles[i] = "background-color: #ff4d4d; color: white; font-weight: bold;"
+                styles[i] = "background-color: white; color: red; font-weight: bold;"
             elif "Low Sweep" in val:
-                styles[i] = "background-color: #00cc66; color: black; font-weight: bold;"
+                styles[i] = "background-color: white; color: green; font-weight: bold;"
     return styles
 
 
